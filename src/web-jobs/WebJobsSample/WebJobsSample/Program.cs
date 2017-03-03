8838a9531bc2867e8f26422b780fe8933b37a011
pihai <philipp-haider@aon.at>
2017-03-03 11:31:42 +0100

    implemented web jobs example in csharp and fsharp

---
+++ src/web-jobs/WebJobsSample/WebJobsSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace WebJobsSample {
+  public class SensorData {
+    public string SensorId { get; set; }
+    public string Timestamp { get; set; }
+    public double Value { get; set; }
+  }
+  class Program {
+    static void Main() { new JobHost().Start(); Console.Read(); }
+  }
+  public class Functions {
+    public static void ProcessSensorData(
+      [QueueTrigger("sensorqueue")] SensorData sensorData,
+      [Blob("values/{SensorId}/{Timestamp}")] out string sensorValue,
+      [Queue("aggregationqueue")] out SensorData aggregationQueue
+    ) {
+      sensorValue = sensorData.Value.ToString();
+      aggregationQueue = sensorData;
+    }
+  }
+}
---
+++ src/web-jobs/WebJobsSample/WebJobsSampleFS/Program.fs
@@ -0,0 +1,20 @@
+﻿namespace Test
+
+open System
+open System.Runtime.InteropServices
+open Microsoft.Azure.WebJobs
+open Microsoft.Azure.WebJobs
+
+module Functions = 
+  type SensorDate = { SensorId: string; Timestamp: string; Value: double }
+  let processSensorData 
+    ([<QueueTrigger("sensorqueue")>] sensorData : SensorDate,
+     [<Blob("values/{SensorId}/{Timestamp}"); Out>] sensorValue : string byref,
+     [<Queue("aggregationqueue"); Out>] aggregationQueue : SensorDate byref) = 
+     sensorValue <- string sensorData.Value
+     aggregationQueue <- sensorData
+
+module Program =
+  let [<EntryPoint>] main args = 
+    (new JobHost()).Start()
+    Console.Read() |> fun _ -> 0
